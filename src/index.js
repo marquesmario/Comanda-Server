2d783aaaee33acdb8ded169d57c027e1f1328100
Mário Marques <marinhomarqs@gmail.com>
2019-10-22 12:26:34 -0300

    Only stay

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+const path = require('path');
+
+const app = express();
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+});
+
+app.set('view engine', 'njk');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('index', { nome: 'Mário' });
+});
+
+app.listen(3000);
